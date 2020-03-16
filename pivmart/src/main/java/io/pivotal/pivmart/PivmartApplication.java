9187934c4320d9fca80bcf373e3a480ddb57e4bd
Adam Bray <adam.bray@gmail.com>
2020-03-16 16:42:59 -0400

    experiment

--- pivmart/src/main/java/io/pivotal/pivmart/PivmartApplication.java
+++ pivmart/src/main/java/io/pivotal/pivmart/PivmartApplication.java
@@ -1,13 +1,15 @@
 package io.pivotal.pivmart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PivmartApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(PivmartApplication.class, args);
+        SpringApplication app = new SpringApplication(PivmartApplication.class);
+        app.setWebApplicationType(WebApplicationType.REACTIVE);
+        app.run(args);
     }
 
 }
