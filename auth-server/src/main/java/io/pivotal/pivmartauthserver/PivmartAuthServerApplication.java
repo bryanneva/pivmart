c874436368550ef8c2a688b00d71a6658ca71365
Adam Bray <adam.bray@gmail.com>
2020-08-28 09:34:10 -0400

    initial commit with auth server

---
+++ auth-server/src/main/java/io/pivotal/pivmartauthserver/PivmartAuthServerApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.pivmartauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivmartAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivmartAuthServerApplication.class, args);
+	}
+
+}
---
+++ auth-server/src/test/java/io/pivotal/pivmartauthserver/PivmartAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.pivmartauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivmartAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
